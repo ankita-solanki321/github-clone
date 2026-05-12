4373812a06c8a2e17520160b214e85dea2ee9277
Ankita Solanki <ankita@gmail.com>
2026-05-12 18:40:11 +0530

    creating dashboard for suggested repository

--- frontend/src/components/dashboard/Dashboard.jsx
+++ frontend/src/components/dashboard/Dashboard.jsx
@@ -1,6 +1,41 @@
-import React from 'react'; 
+import React, { useState, useEffect } from "react";
 
-const Dashboard = () =>{
-   return <h1>Dashboard</h1>
+const Dashboard = () => {
+  const [repositories, setRepositories] = useState([]);
+  const [searchQuery, setSearchQuery] = useState("");
+  const [suggestedRepositories, setSuggestedRepositories] = useState([]);
+  const [searchResults, setSearchResults] = useState([]);
+
+useEffect(() => {
+    const userId = localStorage.getItem("userId");
+
+    const fetchRepositories = async () => {
+      try {
+        const response = await fetch(
+          `http://localhost:3000/repo/user/${userId}`
+        );
+        const data = await response.json();
+        console.log("Repo found:", data);
+        setRepositories(data.repositories);
+        
+      } catch (err) {
+        console.error("Error while fecthing repositories: ", err);
+      }
+    };
+
+    const fetchSuggestedRepositories = async () => {
+      try {
+        const response = await fetch(`http://localhost:3000/repo/all`);
+        const data = await response.json();
+        setSuggestedRepositories(data);
+        console.log(suggestedRepositories);
+      } catch (err) {
+        console.error("Error while fecthing repositories: ", err);
+      }
+    };
+
+    fetchRepositories();
+    fetchSuggestedRepositories();
+  }, []);
 }
 export default Dashboard;
